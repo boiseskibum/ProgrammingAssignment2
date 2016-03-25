933548b4c6cf2b707f146bc4c007e892c6ceb589
boiseskibum <boiseskibum@gmail.com>
2016-03-24 20:44:33 -0600

    Wrote makeCacheMatrix and cacheSolve functions
    Completed out the bodies of the makeCacheMatrix, cacheSolve functions,
    and tested them

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are utilized to reduce the cost of cmoputation required for a 
+## Matrix Inversion if it is done repeatedly.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invertedMatrix <- NULL
+        
+        #create the functions
+        set <- function(y) {
+                x <<- y
+                invertedMatrix <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) invertedMatrix <<- inverse
+        getInverse <- function() invertedMatrix
+        
+        #this puts the functions into a list and returns them
+        list(set = set, get = get,
+                setInverse = setInverse,
+                getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## will retrieve the inverse from the cache instead of doing it again.
 
 cacheSolve <- function(x, ...) {
+  
+        
+        invertedMatrix <- x$getInverse()
+        
+        # return the value if it is already cached
+        if(!is.null(invertedMatrix)) {
+                message("getting cached data")
+                return(invertedMatrix)
+        }
+        
+        matrixData <- x$get()
+        
+        ## added some error handling I saw from another solution
+        tryCatch( {
+                invertedMatrix <- solve(matrixData, ...)
+        },
+        error = function(e) {
+                message("SRT Error: ")
+                message(e)
+                
+                return(NA)
+        },
+        warning = function(e) {
+                message("SRT Warning: ")
+                message(e)
+                
+                return(NA)
+        },
+        finally = {
+                # set inverted matrix in cache
+                x$setInverse(invertedMatrix)
+        } )        
+                
+ 
         ## Return a matrix that is the inverse of 'x'
+        invertedMatrix
+
+  
 }
